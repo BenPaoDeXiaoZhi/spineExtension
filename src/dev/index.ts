39600e729a4e427eabbb7b2097e2f80ce276def8
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-10-26 18:18:58 +0800

    更新 index.ts

--- src/dev/index.ts
+++ src/dev/index.ts
@@ -45,6 +45,8 @@ let skeleton:Spine.Skeleton;
     animationState.setAnimation(0,'Idle_01',true)
     skeleton.scaleX = 0.3
     skeleton.scaleY = 0.3
+    window.skeleton = skeleton
+    window.animationState = animationState
     console.log(atlasLoader, skeletonLoader,skeleton)
     requestAnimationFrame(render)
 })(assetMgr)
