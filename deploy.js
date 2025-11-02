bf2cd8a4eefd90f4d732a5f958549458b64e6b5c
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-02 19:43:45 +0800

    更新 deploy.js

--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
-import { msedge } from 'playwright';
+import { chrome } from 'playwright';
 import fs from 'fs'
 // function getVM(vm){
 //     console.log(vm)
@@ -9,7 +9,7 @@ const uid=token.slice(16)
 console.log("deploy to Project:",pid);
 console.log("using uid:",uid);
 (async () => {
-    const browser = await msedge.launch();
+    const browser = await chrome.launch();
     // Create pages, interact with UI elements, assert values
     const context = await browser.newContext();
     await context.addCookies([
