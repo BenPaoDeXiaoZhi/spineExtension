10010436c0061f3583f4dbd68ef808faab052bff
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-02 16:24:26 +0800

    更新 deploy.js

--- deploy.js
+++ deploy.js
@@ -4,11 +4,23 @@ import { chromium } from 'playwright';
 //     console.log(vm)
 // }
 const pid=process.env.PROJECT_ID
+const token=process.env.CCW_TOKEN||"000000000000000063c2807d669fa967f17f5559"
+const uid=token.slice(16)
 console.log("deploy to Project:",pid);
 (async () => {
     const browser = await chromium.launch();
     // Create pages, interact with UI elements, assert values
     const context = await browser.newContext();
+    await browserContext.addCookies([
+        {
+            name:"token",
+            value:token
+        },
+        {
+            name:"cookie_user_id",
+            value:uid
+        },
+    ]);
     await context.exposeFunction('log', (...dat) => {
         console.log(...dat)
     });
@@ -32,7 +44,7 @@ console.log("deploy to Project:",pid);
             return orig.call(this,self2,...args)
         }
         async function getAssets(){
-            window.log(vm.runtime.gandi)
+            window.log(vm.runtime.gandi.assets)
             await window.exit()
         }
     })
