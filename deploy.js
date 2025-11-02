1bbfa1918478f465fca7040344e900a1d8989608
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-02 13:28:55 +0800

    更新 deploy.js

--- deploy.js
+++ deploy.js
@@ -7,6 +7,24 @@ import { chromium } from 'playwright';
     // Create a new page inside context.
     const page = await context.newPage();
     await page.goto('https://ccw.site/gandi');
+    const vm = await page.evaluate(() => {
+        return new Promise((resolve,reject)=>{
+            const orig = Function.prototype.bind
+            window.vm=null
+            Function.prototype.bind = function(self2,...args){
+                if(
+                    typeof self2=="object" && 
+                    self2 !== null && 
+                    self2.hasOwnProperty("runtime")
+                    ){
+                        Function.prototype.bind=orig
+                        resolve(self2)
+                    }
+                    return orig.call(this,self2,...args)
+            }
+        })
+    }).then((vm)=>vm);
+    console.log(vm)
     // Dispose context once it's no longer needed.
     await context.close();
     await browser.close();
