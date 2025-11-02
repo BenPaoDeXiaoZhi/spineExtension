d7f8f9fbff2d95e59221789cefd703b04e9e4e36
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-02 14:06:19 +0800

    更新 deploy.js

--- deploy.js
+++ deploy.js
@@ -10,26 +10,6 @@ import { chromium } from 'playwright';
     console.log(await page.evaluate(()=>document.title))
     const buffer = await page.screenshot();
     console.log(buffer.toString('base64'))
-    const vm = await page.evaluate(() => {
-        window.location.reload()
-        return new Promise((resolve,reject)=>{
-            setTimeout(()=>reject(),20000)
-            const orig = Function.prototype.bind
-            window.vm=null
-            Function.prototype.bind = function(self2,...args){
-                if(
-                    typeof self2=="object" && 
-                    self2 !== null && 
-                    self2.hasOwnProperty("runtime")
-                    ){
-                        Function.prototype.bind=orig
-                        resolve(self2)
-                    }
-                    return orig.call(this,self2,...args)
-            }
-        })
-    }).then((vm)=>vm);
-    console.log(vm)
     // Dispose context once it's no longer needed.
     await context.close();
     await browser.close();
