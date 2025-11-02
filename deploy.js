332f251d58d1f72905fc4aff8fe7522bbc9724f3
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-02 16:24:48 +0800

    更新 deploy.js

--- deploy.js
+++ deploy.js
@@ -7,6 +7,7 @@ const pid=process.env.PROJECT_ID
 const token=process.env.CCW_TOKEN||"000000000000000063c2807d669fa967f17f5559"
 const uid=token.slice(16)
 console.log("deploy to Project:",pid);
+console.log("using uid:",uid);
 (async () => {
     const browser = await chromium.launch();
     // Create pages, interact with UI elements, assert values
