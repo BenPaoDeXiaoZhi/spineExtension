d1ea349f9d85a217faefb553c376331d743c280e
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-02 13:21:39 +0800

    更新 deploy.js

--- deploy.js
+++ deploy.js
@@ -1,7 +1,13 @@
 import { chromium } from 'playwright';
 
 (async () => {
-  const browser = await chromium.launch();
-  // Create pages, interact with UI elements, assert values
-  await browser.close();
+    const browser = await chromium.launch();
+    // Create pages, interact with UI elements, assert values
+    const context = await browser.newContext();
+    // Create a new page inside context.
+    const page = await context.newPage();
+    await page.goto('https://ccw.site/gandi');
+    // Dispose context once it's no longer needed.
+    await context.close();
+    await browser.close();
 })();
