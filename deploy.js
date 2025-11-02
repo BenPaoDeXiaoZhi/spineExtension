f5a9c17427f85a4a757975b46b6780306050399e
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-02 15:27:22 +0800

    更新 deploy.js

--- deploy.js
+++ deploy.js
@@ -8,14 +8,16 @@ import { chromium } from 'playwright';
     const browser = await chromium.launch();
     // Create pages, interact with UI elements, assert values
     const context = await browser.newContext();
+    await context.exposeFunction('emitVM', (vm) =>
+        console.log(vm)
+        await context.close();
+        await browser.close();
+    );
+    context.addInitScript(()=>{
+        window.emitVM("acb")
+    })
     // Create a new page inside context.
     const page = await context.newPage();
     await page.goto('https://ccw.site/gandi');
-    console.log(await page.evaluate(()=>{
-        const ul = document.querySelector('.gandi_vertical-bar_bar_Tsvpu').firstChild
-        return ul.children[2].innerHTML
-    }))
     // Dispose context once it's no longer needed.
-    await context.close();
-    await browser.close();
 })();
