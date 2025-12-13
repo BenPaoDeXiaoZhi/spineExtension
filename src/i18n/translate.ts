2ddcae32675dc230e3c7bd37e73660802f8f9cf5
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-12-13 18:21:38 +0800

    更新 translate.ts

--- src/i18n/translate.ts
+++ src/i18n/translate.ts
@@ -1,7 +1,8 @@
 import zh_cn from './zh_cn';
 import en from './en';
 export type Id = keyof typeof en & keyof typeof zh_cn;
-export function getTranslate(runtime: any) {
+export type TranslateFn = (id: Id, args?: object) => string;
+export function getTranslate(runtime: any): TranslateFn {
     const fmt = runtime.getFormatMessage({ 'zh-cn': zh_cn, en });
     return function (id: Id, args?: object) {
         return fmt(
