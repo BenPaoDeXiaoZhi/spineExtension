6d437cc36f00e850678cb1d52b5d6039f3e41277
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-01-17 11:24:11 +0800

    更新 index.ts

--- src/index.ts
+++ src/index.ts
@@ -239,6 +239,7 @@ class SpineExtension extends SimpleExt {
                         defaultValue: 'Idle_01',
                     },
                     LOOP: {
+                        type: ArgumentType.BOOLEAN
                         menu:"BOOLEAN"
                     },
                 },
@@ -260,8 +261,8 @@ class SpineExtension extends SimpleExt {
             },
             BOOLEAN: {
                 items:[
-                    {text: "进行",  value: true},
-                    {text: "不",  value: false}
+                    {text: "进行",  value: "true"},
+                    {text: "不",  value: "false"}
                 ],
                 acceptReporters: true,
             },
