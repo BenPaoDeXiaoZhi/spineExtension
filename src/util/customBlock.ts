a1c8445975cb30752714b20e2babdec73bd75d5c
BenPaoDeXiaoZhi <mengfanzhi1232022@163.com>
2026-01-23 23:00:42 +0800

    add static file

--- src/util/customBlock.ts
+++ src/util/customBlock.ts
@@ -1,7 +1,5 @@
 import { Ext } from '..';
 import { setupGetSth } from './customBlocks/getSth';
-import { setupPos } from './customBlocks/setRelativePos';
 export function setupCustomBlocks(ext: Ext, NS: string) {
     setupGetSth(ext, NS);
-    // setupPos(ext, NS);
 }
