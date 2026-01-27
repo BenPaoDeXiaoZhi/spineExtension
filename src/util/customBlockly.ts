0b79c0be7e4601b4a8b69584b510621f58c78ccf
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-01-27 08:16:24 +0800

    更新 customBlockly.ts

--- src/util/customBlockly.ts
+++ src/util/customBlockly.ts
@@ -47,3 +47,31 @@ export function registerConnectionCallback(
         origConnect.call(this, otherConn);
     };
 }
+
+export function createVMShadow(block: BlockSvg){
+    // skeleton
+    const blockDat = {
+        id: block.id, // Block ID
+        opcode: block.type, // For execution, "event_whengreenflag".
+        inputs: {}, // Inputs to this block and the blocks they point to.
+        fields: {}, // Fields on this block and their values.
+        next: null, // Next block in the stack, if one exists.
+        topLevel: false, // If this block starts a stack. 对于shadow，这个应为false
+        parent: block.outputConnection.targetBlock(), // Parent block ID, if available.
+        shadow: true, // If this represents a shadow/slot.
+        // powered by xigua start
+        hidden: false,
+        locked: false,
+        collapsed: false,
+        // powered by xigua end
+        x: block.x, // X position of script, if top-level.
+        y: block.y // Y position of script, if top-level.
+    };
+    if(block.type == "text"){
+        blockDat.fields["TEXT"] = block.getFieldValue("TEXT");
+    }
+    if(block.type == "math_number"){
+        blockDat.fields["NUM"] = block.getFieldValue("NUM");
+    }
+    return blockDat;
+}
